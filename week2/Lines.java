36595ff84111426094b1046627c3aebd4bda5725
KristinaJelyazkova <thesecretgirlbls@abv.bg>
2018-10-14 23:00:15 +0300

    Add files via upload

--- week2/Lines.java
+++ week2/Lines.java
@@ -21,17 +21,27 @@ public class Lines extends Application {
         double width = scene.getWidth();
         double height = scene.getHeight();
 
+        Rectangle rectangle = new Rectangle(0, 0, width, height);
+        rectangle.setFill(Color.BLACK);
+        group.getChildren().add(rectangle);
 
-        for(int startX = 20; startX <= width; startX += 20) {
-            Line line = new Line(startX , height, 0, startX);
-            line.setStroke(Color.GREEN);
-            line.setStrokeWidth(1);
-            group.getChildren().add(line);
-        }
+        drawLines(height, 0, 1, 0, Color.GREEN, width,  group);
+        drawLines(height, width, -1, width, Color.RED, width, group);
+        drawLines(0, 0, -1, width, Color.YELLOW, width, group);
+        drawLines(0, width, 1, 0, Color.BLUE, width, group);
 
         primaryStage.setTitle("Drawing lines");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    private void drawLines(double startY, double endX, double sign, double plusStartX,
+                           Paint color, double width, Group group){
+        for(int startX = 20; startX <= width; startX += 20) {
+            Line line = new Line(startX * sign + plusStartX, startY, endX, startX);
+            line.setStroke(color);
+            line.setStrokeWidth(1);
+            group.getChildren().add(line);
+        }
+    }
 }
